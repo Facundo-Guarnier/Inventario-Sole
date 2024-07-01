2ceeb92408fb9be763040c57ad17097497e07acd
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-01 18:57:17 -0300

    Frontend eliminado

--- Frontend/Inventario/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { AppPagTiendaFisicaComponent } from './paginas/app-pag-tienda-fisica/app-pag-tienda-fisica.component';
-
-export const routes: Routes = [
-    {path: '', component: AppPagTiendaFisicaComponent},
-];
