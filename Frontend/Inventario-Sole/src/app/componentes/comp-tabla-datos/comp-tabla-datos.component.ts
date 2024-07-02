34dce8352a1b43801dd6e1d600c78db03d757d1e
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-02 19:34:13 -0300

    Mejora tabla datos

--- Frontend/Inventario-Sole/src/app/componentes/comp-tabla-datos/comp-tabla-datos.component.ts
+++ Frontend/Inventario-Sole/src/app/componentes/comp-tabla-datos/comp-tabla-datos.component.ts
@@ -8,10 +8,49 @@ import { Component, OnInit, Input } from '@angular/core';
 export class CompTablaDatosComponent implements OnInit {
 
   @Input() productos: any; 
+  cdRef: any;
   
   constructor() { }
 
   ngOnInit(): void {
+    this.productos = [
+      {
+        "idProducto": "AB120",
+        "marca": "Adidas",
+        "descripcion": "Pantalón negro deportivo",
+        "talle": "M",
+        "precio": 80000,
+        "cantidad": 12,
+        "liquidacion": true,
+      },
+      {
+        "idProducto": "AB121",
+        "marca": "Nike",
+        "descripcion": "Remera azul deportiva",
+        "talle": "M",
+        "precio": 45000,
+        "cantidad": 7,
+        "liquidacion": true,
+      },
+      {
+        "idProducto": "AB122",
+        "marca": "AAA",
+        "descripcion": "Remera deportiva Remera azul deportiva Remera  asd asd ada dasd ad aazul  a sda  asd asd 151 81 562deportiva Remera azul deportiva",
+        "talle": "XL",
+        "precio": 900000,
+        "cantidad": 2,
+        "liquidacion": false,
+      },
+      {
+        "idProducto": "AB123",
+        "marca": "Adidas",
+        "descripcion": "Campera",
+        "talle": "M",
+        "precio": 10000,
+        "cantidad": 1,
+        "liquidacion": true,
+      },
+    ]
   }
 
 }
