ec98e754d03d5187debdcf7abc256f309e30e294
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-06 17:29:28 -0300

    Intento 2 de subir fotos. Ahora si abre el explorador de archivos.

--- Frontend/Inventario-Sole/src/app/componentes/comp-botones-flotantes/comp-botones-flotantes.component.ts
+++ Frontend/Inventario-Sole/src/app/componentes/comp-botones-flotantes/comp-botones-flotantes.component.ts
@@ -5,7 +5,7 @@ import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, EventEmitter, 
   templateUrl: './comp-botones-flotantes.component.html',
   styleUrls: ['./comp-botones-flotantes.component.css']
 })
-export class CompBotonesFlotantesComponent implements OnInit, AfterViewInit {
+export class CompBotonesFlotantesComponent implements OnInit {
 
   //! Controlan la visibilidad de los botones
   @Input() mostrarAceptar: boolean = false;
@@ -20,15 +20,14 @@ export class CompBotonesFlotantesComponent implements OnInit, AfterViewInit {
   @Output() clickBorrar = new EventEmitter<void>();
   @Output() clickAgregar = new EventEmitter<void>();
   @Output() clickAgregarFoto = new EventEmitter<void>();
-  
 
-//* ---------------------------------------------------
-
-
+  //* ---------------------------------------------------
+  @Output() archivoSeleccionado = new EventEmitter<Event>();
   @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement> | undefined;
+  //* ---------------------------------------------------
 
 
-//* ---------------------------------------------------
+
 
   ClickAceptar() {
     this.clickAceptar.emit();
@@ -46,55 +45,24 @@ export class CompBotonesFlotantesComponent implements OnInit, AfterViewInit {
     this.clickAgregar.emit();
   }
   
-  // ClickAgregarFoto() {
-  //   this.clickAgregarFoto.emit();
-  // }
+  //! Agregar foto
+  ClickAgregarFoto() {
+    this.clickAgregarFoto.emit();
+    if (this.fileInput) {
+      this.fileInput.nativeElement.click();
+    }
+  }
+  ArchivoSeleccionado(event: Event) {
+    this.archivoSeleccionado.emit();
+  }
   
-  constructor(private cdRef: ChangeDetectorRef) { }
+  
+  constructor(
+    private cdRef: ChangeDetectorRef
+  ) { }
   
   ngOnInit(): void {
     this.cdRef.detectChanges(); //! Fuerza a Angular a realizar una verificación de cambios en este componente y sus hijos.
   }
-
-
-
-//* ---------------------------------------------------
-
-ngAfterViewInit() {
-  if (this.fileInput) {
-    this.fileInput.nativeElement.style.display = 'none';
-  }
-}
-
-ClickAgregarFoto() {
-  console.log('Click en Agregar Foto');
-  if (!this.fileInput) {
-    console.error('No se ha cargado el input');
-    return;
-  }
-
-  if (this.isMobile()) {
-    this.fileInput.nativeElement.capture = 'environment';
-  }
-  this.fileInput.nativeElement.click();
-}
-
-onFileSelected(event: Event) {
-  const input = event.target as HTMLInputElement;
-  if (!input.files) {
-    return;
-  }
-
-  const file = input.files[0];
-  if (file) {
-    console.log('Archivo seleccionado:', file.name);
-    // TODO: Agregar aquí la lógica para subir la imagen
-  }
-}
-
-private isMobile(): boolean {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
 
 }
--- Frontend/Inventario-Sole/src/app/paginas/productos/crear/crear.component.ts
+++ Frontend/Inventario-Sole/src/app/paginas/productos/crear/crear.component.ts
@@ -5,12 +5,60 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
   templateUrl: './crear.component.html',
   styleUrls: ['./crear.component.css'],
 })
-export class PagProductosCrearComponent implements OnInit {
+export class PagProductosCrearComponent implements OnInit, AfterViewInit {
+
+
+  //* ---------------------------------------------------
+  @ViewChild('fileInput') fileInput: ElementRef<HTMLInputElement> | undefined;
+  //* ---------------------------------------------------
+
+
 
 
   constructor() {}
 
   ngOnInit(): void {}
 
+
+
+
+
+  //* ---------------------------------------------------
+  ngAfterViewInit() {
+    if (this.fileInput) {
+      this.fileInput.nativeElement.style.display = 'none';
+    }
+  }
+
+  ClickAgregarFoto() {
+    console.log('Click en Agregar Foto');
+    if (!this.fileInput) {
+      console.error('No se ha cargado el input');
+      return;
+    }
+
+    if (this.isMobile()) {
+      this.fileInput.nativeElement.capture = 'environment';
+    }
+    this.fileInput.nativeElement.click();
+  }
+
+  archivoSeleccionado(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+    if (file) {
+      console.log('Archivo seleccionado:');
+      // TODO: Agregar aquí la lógica para subir la imagen
+    }
+  }
+
+  private isMobile(): boolean {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+  //* ---------------------------------------------------
 
 }
