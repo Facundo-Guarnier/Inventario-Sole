6917dc2d883b53c7c33f5657e2f0f0829e9a519f
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-06 19:51:33 -0300

    Mejora en qué mostrar y qué no en la barra lateral

--- Frontend/Inventario-Sole/src/app/componentes/comp-barra-lateral/comp-barra-lateral.component.ts
+++ Frontend/Inventario-Sole/src/app/componentes/comp-barra-lateral/comp-barra-lateral.component.ts
@@ -12,31 +12,38 @@ export class CompBarraLateralComponent implements OnInit {
   @Input() filtrosLista: Filtro[] = [];
   @Input() filtrosCheckbox: string[] = [];
 
-  pagActual: string = '';
-
+  urlActual: string[] = this.router.url.split('?')[0].split('/').slice(1);
   
   mostrarBusqueda(): boolean {
     //! Activa o desactiva la barra de busqueda en base a la pag actual.
-    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.pagActual);
+    if (this.urlActual.length > 1) {
+      return false
+    }
+    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
   }
   
   mostrarFiltroListaSeleccion(): boolean {
     //! Activa o desactiva el filtro de lista de seleccion en base a la pag actual.
-    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.pagActual);
+    if (this.urlActual.length > 1) {
+      return false
+    }
+    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
   }
 
   mostrarFiltroCheckbox(): boolean {
     //! Activa o desactiva el filtro de checkbox en base a la pag actual.
-    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.pagActual);
+    if (this.urlActual.length > 1) {
+      return false
+    }
+    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
   }
 
   mostrarDetalleLateral(): boolean {
     //! Activa o desactiva el detalle lateral en base a la página actual.
-    //! Evita aparecer en prod/crear
-    const urlSegment = this.router.url.split('/')[1].split('?')[0];   
-    const isProdCrear = this.router.url.startsWith('/prod/crear');
-
-    return urlSegment === 'prod' && !isProdCrear;
+    if (this.urlActual.length > 1) {
+      return ['prod'].includes(this.urlActual[0]);
+    }
+    return false;
 }
 
   constructor(
@@ -44,15 +51,17 @@ export class CompBarraLateralComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.pagActual = this.router.url.split('/')[1].split('?')[0];
-
+    
+    console.log("URL actual: " + this.urlActual)
+    //TODO Borrar, solo para pruebas
     this.filtrosLista = [
       {nombre: 'Filtro 1', opciones: ['Opción 1', 'Opción 2']},
       {nombre: 'Filtro 2', opciones: ['Opción 1', 'Opción 2', 'Opción 3']},
       {nombre: 'Filtro 3', opciones: ['Opción 1', 'Opción 2', 'Opción 3', 'Opción 4']},
       {nombre: 'Filtro 4', opciones: ['Opción 1', 'Opción 2']}
     ]
-  
+    
+    //TODO Borrar, solo para pruebas
     this.filtrosCheckbox = [
       'Checkbox 1', 
       'Checkbox 2', 
