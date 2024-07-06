181ecb24fe61f0ae636a6e6e85a6d2a6682619dd
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-06 19:10:17 -0300

    Casi terminado el capo para mostar fotos

--- Frontend/Inventario-Sole/src/app/componentes/comp-campo-fotos/comp-campo-fotos.component.ts
+++ Frontend/Inventario-Sole/src/app/componentes/comp-campo-fotos/comp-campo-fotos.component.ts
@@ -6,23 +6,34 @@ import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output } from '@ang
   styleUrls: ['./comp-campo-fotos.component.css']
 })
 export class CompCampoFotosComponent implements OnInit {
-
+  
   @Output() clickAgregarFoto = new EventEmitter<void>();
   @Output() archivoSeleccionado = new EventEmitter<Event>();
-
-
+  
+  listaFotos: string[] = [];
+  
   constructor(
     private cdRef: ChangeDetectorRef
   ) { }
-
+  
   ngOnInit(): void {
+    this.listaFotos = [
+      "foto1",
+      "foto2",
+      "foto3",
+      "foto4",
+      "foto5",
+      "foto6",
+      "foto7",
+      "foto8",
+      "foto9",
+    ];
   }
-
   
   ClickAgregarFoto() {
     this.clickAgregarFoto.emit();
   }
-
+  
   ArchivoSeleccionado(event: Event) {
     this.archivoSeleccionado.emit();
   }
