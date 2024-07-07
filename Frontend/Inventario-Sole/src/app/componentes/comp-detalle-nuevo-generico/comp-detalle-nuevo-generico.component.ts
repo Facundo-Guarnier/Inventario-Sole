bde6dfc6879e6293d737b52e362a4e5dafc32e4e
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-07 10:15:54 -0300

    Funcionamiento de Nuevo detalle generico

--- Frontend/Inventario-Sole/src/app/componentes/comp-detalle-nuevo-generico/comp-detalle-nuevo-generico.component.ts
+++ Frontend/Inventario-Sole/src/app/componentes/comp-detalle-nuevo-generico/comp-detalle-nuevo-generico.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Campo } from '../../interfaces/campo.interface'
 
 @Component({
   selector: 'app-comp-detalle-nuevo-generico',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comp-detalle-nuevo-generico.component.css']
 })
 export class CompDetalleNuevoMovComponent implements OnInit {
-
+  
+  @Input() campos: Campo[] = [];  //! Nombre, identificador y tipo. Ej: "Cantidad", "cantidad", "input-number"
+  
   constructor() { }
 
   ngOnInit(): void {
---
+++ Frontend/Inventario-Sole/src/app/interfaces/campo.interface.ts
@@ -0,0 +1,5 @@
+export interface Campo {
+    nombre: string;
+    identificador: string;
+    tipo: string;   //! textarea-text, input-text, input-number
+}
