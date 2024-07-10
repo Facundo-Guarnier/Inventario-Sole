55fc0635473d3d6e6cea8669b12318d26c6296a9
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-10 20:41:48 -0300

    Corrección al crear el admin

--- Backend/App/__init__.py
+++ Backend/App/__init__.py
@@ -32,7 +32,9 @@ def initialize_database(app):
             print(f"Colección '{collection}' ya existente.")
     
     admin = mongo.db.usuarios.find_one({"roles": {"$in": ["admin"]}})    
+    print(admin)
     if admin == None:
+        mongo.db.usuarios.delete_one({"alias": "admin"})
         from werkzeug.security import generate_password_hash
         mongo.db.usuarios.insert_one(
             {
