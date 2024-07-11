0dea6fa8fb5e9db4ba2dc6675d6fbe4467b2326e
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-11 19:22:55 -0300

    Mejora en editar venta

--- Frontend/Inventario-Sole/src/app/componentes/comp-venta-lista-prod/comp-venta-lista-prod.component.ts
+++ Frontend/Inventario-Sole/src/app/componentes/comp-venta-lista-prod/comp-venta-lista-prod.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Campo } from 'src/app/interfaces/campo.interface';
 
 @Component({
@@ -6,18 +6,35 @@ import { Campo } from 'src/app/interfaces/campo.interface';
   templateUrl: './comp-venta-lista-prod.component.html',
   styleUrls: ['./comp-venta-lista-prod.component.css']
 })
-export class CompVentaListaProdComponent implements OnInit {
+export class CompVentaListaProdComponent implements OnInit, OnChanges  {
 
   @Output() datosRecolectados = new EventEmitter<any[]>();
   @Input() estilo: string = "normal";  //! "normal" o "compacto"
   @Input() titulo: string = "Nuevo Detalle";
   @Input() campos: Campo[] = [];  //! Nombre, identificador y tipo. Ej: "Cantidad", "cantidad", "input-number"
   
-  productos: any[] = [{}]
+  @Input() datosOriginales: any[] = [];
+
+  productos: any[] = [{}];
   
   constructor() { }
 
   ngOnInit(): void {
+    this.actualizarProductos();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["datosOriginales"]) {
+      this.actualizarProductos();
+    }
+  }
+
+  actualizarProductos(): void {
+    if (this.datosOriginales && this.datosOriginales.length > 0) {
+      this.productos = [...this.datosOriginales];
+    }
+
+    console.log('Datos originales:', this.productos);
   }
 
   agregarProducto() {
@@ -30,7 +47,7 @@ export class CompVentaListaProdComponent implements OnInit {
 
   recolectarDatos() {
     const datosRecolectados = this.productos.map((producto, index) => {
-      const datosProducto: any = {};
+      const datosProducto: any = { ...producto };  // Comenzar con los datos existentes
       this.campos.forEach(campo => {
         const elemento = document.getElementById(`${campo.identificador}_${index}`) as HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement;
         if (elemento) {
@@ -41,7 +58,7 @@ export class CompVentaListaProdComponent implements OnInit {
       });
       return datosProducto;
     });
-    
+
     this.datosRecolectados.emit(datosRecolectados);
     return datosRecolectados;
   }
--- Frontend/Inventario-Sole/src/app/paginas/ventas/detalle-editar/detalle-editar.component.ts
+++ Frontend/Inventario-Sole/src/app/paginas/ventas/detalle-editar/detalle-editar.component.ts
@@ -19,6 +19,7 @@ export class PagVentasDetalleEditarComponent implements OnInit {
   titulo1 = "Detalle de la venta";
   campos1: Campo[] = [
     { nombre: "ID venta", identificador: "id", tipo: "readonly", valor: this.router.url.split("?")[0].split('/').pop()},
+    { nombre: "Fecha", identificador: "fecha", tipo: "readonly"},
     { nombre: "Cliente", identificador: "cliente", tipo: "input-text"},
     { nombre: "Tienda", identificador: "tienda", tipo: "selector", opciones: ["Fisica", "Online"] },
     { nombre: "Método", identificador: "metodo", tipo: "textarea-text"},
@@ -27,7 +28,7 @@ export class PagVentasDetalleEditarComponent implements OnInit {
 
   titulo2 = "Productos";
   campos2: Campo[] = [
-    { nombre: "ID producto", identificador: "idProducto", tipo: "input-text" },
+    { nombre: "ID producto", identificador: "id", tipo: "input-text" },
     { nombre: "Cantidad", identificador: "cantidad", tipo: "input-number" },
     { nombre: "Precio", identificador: "precio", tipo: "input-number" },
     { nombre: "Comentario", identificador: "comentario", tipo: "textarea-text"}
@@ -35,6 +36,8 @@ export class PagVentasDetalleEditarComponent implements OnInit {
 
   productos: any[] = [];
   detalleventa: any[] = [];
+
+  datosOriginalesProductos: any[] = [];
 
   constructor(
     private router: Router,
@@ -47,14 +50,15 @@ export class PagVentasDetalleEditarComponent implements OnInit {
         let datos = res["msg"][0]
         
         this.campos1[0].valor = datos["id"];
-        this.campos1[1].valor = datos["cliente"];
-        this.campos1[2].valor = datos["tienda"];
-        this.campos1[3].valor = datos["metodo"];
-        this.campos1[4].valor = datos["comentario"];
+        this.campos1[1].valor = datos["fecha"];
+        this.campos1[2].valor = datos["cliente"];
+        this.campos1[3].valor = datos["tienda"];
+        this.campos1[4].valor = datos["metodo"];
+        this.campos1[5].valor = datos["comentario"];
 
-        this.productos = datos["productos"];
+        this.datosOriginalesProductos = datos["productos"];
       },
-      
+
       (err: any) => {
         console.log('Error al buscar la venta:', err);
       }
