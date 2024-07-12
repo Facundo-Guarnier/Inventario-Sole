1b1bed1390465d3e2e39ce66bd8e9035d73176fa
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-07-12 18:24:06 -0300

    Boton logout

--- Frontend/Inventario-Sole/src/app/componentes/comp-barra-lateral/comp-barra-lateral.component.ts
+++ Frontend/Inventario-Sole/src/app/componentes/comp-barra-lateral/comp-barra-lateral.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Filtro } from '../../interfaces/filtro.interface'
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-comp-barra-lateral',
@@ -9,47 +10,19 @@ import { Filtro } from '../../interfaces/filtro.interface'
 })
 export class CompBarraLateralComponent implements OnInit {
 
+  //! Valores del componente padre
   @Input() filtrosLista: Filtro[] = [];
   @Input() filtrosCheckbox: string[] = [];
-
+  
   urlActual: string[] = this.router.url.split('?')[0].split('/').slice(1);
   
-  mostrarBusqueda(): boolean {
-    //! Activa o desactiva la barra de busqueda en base a la pag actual.
-    if (this.urlActual.length > 1) {
-      return false
-    }
-    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
-  }
+  //* ------------------------------------------------------------
   
-  mostrarFiltroListaSeleccion(): boolean {
-    //! Activa o desactiva el filtro de lista de seleccion en base a la pag actual.
-    if (this.urlActual.length > 1) {
-      return false
-    }
-    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
-  }
-
-  mostrarFiltroCheckbox(): boolean {
-    //! Activa o desactiva el filtro de checkbox en base a la pag actual.
-    if (this.urlActual.length > 1) {
-      return false
-    }
-    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
-  }
-
-  mostrarDetalleLateral(): boolean {
-    //! Activa o desactiva el detalle lateral en base a la página actual.
-    // if (this.urlActual.length > 1) {
-    //   return ["prod"].includes(this.urlActual[0]) && this.urlActual[1].includes('detalle-editar');
-    // }
-    return false;
-}
-
   constructor(
-    private router: Router
+    private router: Router,
+    public authService: AuthService,
   ) { }
-
+  
   ngOnInit(): void {
     
     //TODO Borrar, solo para pruebas
@@ -68,5 +41,43 @@ export class CompBarraLateralComponent implements OnInit {
       'Checkbox 4'
     ]
   }
-
+  
+  //T* Funciones
+  //! Para mostar u ocultar los filtros
+  mostrarBusqueda(): boolean {
+    //! Activa o desactiva la barra de busqueda en base a la pag actual.
+    if (this.urlActual.length > 1) {
+      return false
+    }
+    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
+  }
+  
+  mostrarFiltroListaSeleccion(): boolean {
+    //! Activa o desactiva el filtro de lista de seleccion en base a la pag actual.
+    if (this.urlActual.length > 1) {
+      return false
+    }
+    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
+  }
+  
+  mostrarFiltroCheckbox(): boolean {
+    //! Activa o desactiva el filtro de checkbox en base a la pag actual.
+    if (this.urlActual.length > 1) {
+      return false
+    }
+    return ['tf', 'to', 'gc', 'reg', 'ven', 'mov'].includes(this.urlActual[0]);
+  }
+  
+  mostrarDetalleLateral(): boolean {
+    //! Activa o desactiva el detalle lateral en base a la página actual.
+    // if (this.urlActual.length > 1) {
+    //   return ["prod"].includes(this.urlActual[0]) && this.urlActual[1].includes('detalle-editar');
+    // }
+    return false;
+}
+  
+  //! Boton
+  logout() {
+    this.authService.logout();
+  }
 }
