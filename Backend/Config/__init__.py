aff3d2d9bbafed75c081a975d75130d44e52a1ff
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-08-03 14:12:15 -0300

    Arreglo en Config

--- Backend/Config/__init__.py
+++ Backend/Config/__init__.py
@@ -5,28 +5,24 @@ def is_running_in_kubernetes():
     return os.path.exists('/var/run/secrets/kubernetes.io/serviceaccount')
 
 class Config:
-    def __init__(self):
-        
-        #! Revisar si se está ejecutando en Kubernetes o no
-        if not is_running_in_kubernetes():
-            load_dotenv()
-        
-        self.SECRET_KEY = os.environ.get('SECRET_KEY')
-        self.MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
-        self.MONGO_URI = os.environ.get('MONGO_URI')
-        self.MONGO_COLLECTIONS = os.environ.get('MONGO_COLLECTIONS', '').split(',')
-        self.CONTRA_ADMIN = os.environ.get('CONTRA_ADMIN')
-        self.JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-        
-        self.BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-        
-        #! Imágenes
-        self.UPLOAD_FOLDER = os.path.join(self.BASE_DIR, 'uploads')
-        self.IMG_SIZE = tuple(map(int, os.environ.get('IMG_SIZE', '960,960').split(',')))
-        self.IMG_QUALITY = int(os.environ.get('IMG_QUALITY', '70'))
-        
-        #! Backup de la DB
-        self.CONTRA_BACKUP = os.environ.get('CONTRA_BACKUP')
-
-    def get(self, key, default=None):
-        return getattr(self, key, default)
+    
+    #! Revisar si se está ejecutando en Kubernetes o no
+    if not is_running_in_kubernetes():
+        load_dotenv()
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
+    MONGO_URI = os.environ.get('MONGO_URI')
+    MONGO_COLLECTIONS = os.environ.get('MONGO_COLLECTIONS', '').split(',')
+    CONTRA_ADMIN = os.environ.get('CONTRA_ADMIN')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    
+    BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    
+    #! Imágenes
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+    IMG_SIZE = tuple(map(int, os.environ.get('IMG_SIZE', '960,960').split(',')))
+    IMG_QUALITY = int(os.environ.get('IMG_QUALITY', '70'))
+    
+    #! Backup de la DB
+    CONTRA_BACKUP = os.environ.get('CONTRA_BACKUP')
