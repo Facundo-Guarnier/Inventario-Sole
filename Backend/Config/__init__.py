dbeab838e0ae0fbc72b597b789e11edc249e667d
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2024-08-03 13:11:10 -0300

    Cargar las variables de entorno primero de k8s y despues del .env

--- Backend/Config/__init__.py
+++ Backend/Config/__init__.py
@@ -1,39 +1,32 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+def is_running_in_kubernetes():
+    return os.path.exists('/var/run/secrets/kubernetes.io/serviceaccount')
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    
-    MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
-    MONGO_URI = os.environ.get('MONGO_URI')
-    MONGO_COLLECTIONS = os.environ.get('MONGO_COLLECTIONS', '').split(',')
-    CONTRA_ADMIN = os.environ.get('CONTRA_ADMIN')
-    
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    
-    BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    
-    #! Imágenes
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-    IMG_SIZE = tuple(map(int, os.environ.get('IMG_SIZE').split(',')))
-    IMG_QUALITY = int(os.environ.get('IMG_QUALITY'))
-    
-    #! Backup de la DB
-    CONTRA_BACKUP = os.environ.get('CONTRA_BACKUP')
-    
-    # print("Configuración cargada.", \
-    #     f"SECRET_KEY {SECRET_KEY}", \
-    #     f"MONGO_DBNAME {MONGO_DBNAME}", \
-    #     f"MONGO_URI {MONGO_URI}", \
-    #     f"MONGO_COLLECTIONS {MONGO_COLLECTIONS}", \
-    #     f"CONTRA_ADMIN {CONTRA_ADMIN}", \
-    #     f"JWT_SECRET_KEY {JWT_SECRET_KEY}", \
-    #     f"BASE_DIR {BASE_DIR}", \
-    #     f"UPLOAD_FOLDER {UPLOAD_FOLDER}", \
-    #     f"IMG_SIZE {IMG_SIZE}", \
-    #     f"IMG_QUALITY {IMG_QUALITY}", \
-    #     f"CONTRA_BACKUP {CONTRA_BACKUP}", \
-    #     sep='\n'
-    # )
+    def __init__(self):
+        
+        #! Revisar si se está ejecutando en Kubernetes o no
+        if not is_running_in_kubernetes():
+            load_dotenv()
+        
+        self.SECRET_KEY = os.environ.get('SECRET_KEY')
+        self.MONGO_DBNAME = os.environ.get('MONGO_DBNAME')
+        self.MONGO_URI = os.environ.get('MONGO_URI')
+        self.MONGO_COLLECTIONS = os.environ.get('MONGO_COLLECTIONS', '').split(',')
+        self.CONTRA_ADMIN = os.environ.get('CONTRA_ADMIN')
+        self.JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+        
+        self.BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+        
+        #! Imágenes
+        self.UPLOAD_FOLDER = os.path.join(self.BASE_DIR, 'uploads')
+        self.IMG_SIZE = tuple(map(int, os.environ.get('IMG_SIZE', '960,960').split(',')))
+        self.IMG_QUALITY = int(os.environ.get('IMG_QUALITY', '70'))
+        
+        #! Backup de la DB
+        self.CONTRA_BACKUP = os.environ.get('CONTRA_BACKUP')
+
+    def get(self, key, default=None):
+        return getattr(self, key, default)
